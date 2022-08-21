93b237d3064ff77dbf640d73cb1564cb616f9522
Kemal <i.kemal.karabulut@gmail.com>
2022-08-21 19:38:59 +0200

    Rock paper and scissors game is added.

---
+++ JavaScript/Rock Paper Scissors/Rock.js
@@ -0,0 +1,49 @@
+const getUserChoice = (userInput) => {
+    userInput = userInput.toLowerCase();
+  
+    if (
+      userInput === "rock" ||
+      userInput === "scissors" ||
+      userInput === "paper"
+    ) {
+      return userInput;
+    } else {
+      console.log("You have to pick a proper choice");
+    }
+  };
+  
+  const getComputerChoice = () => {
+    let randomNumber = Math.floor(Math.random() * 3);
+    switch (randomNumber) {
+      case 0:
+        return "rock";
+      case 1:
+        return "paper";
+      case 2:
+        return "scissors";
+    }
+  };
+  
+  
+  const determineWinner = (userChoice, ComputerChoice) => {
+    if (userChoice === computerChoice) {
+      return "Tie";
+    } else if (userChoice === "rock" && computerChoice === "scissors") {
+      return "user wins";
+    } else if (userChoice === "paper" && computerChoice === "rock") {
+      return "User Wins";
+    } else if (userChoice === "scissors" && computerChoice === "rock") {
+      return "User Wins";
+    } else {
+      return "Computer Wins";
+    }
+  };
+  const playGame = function(){
+    userChoice = getUserChoice('rock');
+    console.log(`User choice is ${userChoice}`)
+    computerChoice = getComputerChoice();
+    console.log(`Computer choice is ${computerChoice}`)
+    console.log(determineWinner(userChoice, computerChoice));
+  }
+  playGame();
+  
